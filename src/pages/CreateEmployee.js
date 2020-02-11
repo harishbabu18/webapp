5ad0e860acb8583f8f187a3a10832b1b02f8e55e
teraret <tejas6083@gmail.com>
2020-02-11 15:47:17 +0530

    undo create Employee

--- src/pages/CreateEmployee.js
+++ src/pages/CreateEmployee.js
@@ -1,7 +1,7 @@
 import React from 'react';
-// import { makeStyles } from '@material-ui/core/styles';
+import { makeStyles } from '@material-ui/core/styles';
 import TextField from '@material-ui/core/TextField';
-import { Button, withStyles } from '@material-ui/core';
+import { Button } from '@material-ui/core';
 import Typography from '@material-ui/core/Typography';
 import Grid from '@material-ui/core/Grid';
 import Paper from '@material-ui/core/Paper';
@@ -16,7 +16,7 @@ import AccountCircle from '@material-ui/icons/AccountCircle';
 import Web from '@material-ui/icons/Web';
 import CountrySelect from '../components/CountrySelect'
 
-const useStyles = theme => ({
+const useStyles = makeStyles(theme => ({
   container: {
     display: 'flex',
     flexWrap: 'wrap',
@@ -29,12 +29,10 @@ const useStyles = theme => ({
     width: 200,
   },
 
-});
+}));
 
-class  CreateEmployee extends React {
-
-    render(){
-        const {classes} = this.props;
+export default function CreateEmployee() {
+  const classes = useStyles();
 
   return (
     <div>
@@ -187,6 +185,3 @@ class  CreateEmployee extends React {
     </div>
   );
 }
-}
-
-export default (withStyles)(useStyles)(CreateEmployee);
